33939878b781453fe5349c66cdb729b1573e0df9
Dilitha <diliw101@gmail.com>
2024-09-20 10:07:57 +1200

    Add delete group functionality

--- components/groups.tsx
+++ components/groups.tsx
@@ -32,7 +32,7 @@ export function GroupsView(props: Props) {
 			alert("Email not found");
 			return;
 		}
-		const response = await fetch(form.action, {
+		const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/group/user/add", {
 			method: "POST",
 			body: JSON.stringify({ group_id: +group_id, email, request_email: props.email }),
 			// TODO: Fix this
@@ -47,24 +47,46 @@ export function GroupsView(props: Props) {
 
 	};
 
+	async function deleteGroup(id: string) {
+		const answer = confirm("Are you sure you want to delete this group?");
+		if (!answer) {
+			return;
+		}
+		const group_id = +id;
+		const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/group", {
+			method: "DELETE",
+			body: JSON.stringify({ group_id, user_email: props.email }),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+
+		if (!response.ok) {
+			const text = await response.text();
+			alert("Failed to delete group: " + text);
+		}
+	}
 
 	return (
 		<>
 			{
 				props.groups?.map((group) => (
 					<div key={group.id} className="p-5 m-5 border-2 border-gray-200 rounded-md">
-						<h2 className="text-2xl font-semibold mb-3">{group.name}</h2>
-						<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-3" onClick={() => startSession(group.id, group.name)}>Start session</button>
-						<form method="POST" action={process.env.NEXT_PUBLIC_BACKEND_URL + "/group/user/add"} onSubmit={inviteUser}>
-							<input type="email" name="email" placeholder="Email" className="input input-bordered mr-5" />
-							<input type="hidden" name="group_id" value={group.id} />
-							<button type="submit" className="btn btn-primary">Invite</button>
-						</form>
-
-					</div>
+						<div className="flex flex-row space-x-4">
+							<h2 className="text-2xl font-semibold mb-3">{group.name}</h2>
+							<button className="btn btn-primary" onClick={() => startSession(group.id, group.name)}>Start session</button>
+							<form method="POST" onSubmit={inviteUser}>
+								<input type="email" name="email" placeholder="Email" className="input input-bordered mr-5" />
+								<input type="hidden" name="group_id" value={group.id} />
+								<button type="submit" className="btn btn-secondary">Invite</button>
+							</form>
+							<button className="btn btn-error" onClick={() => deleteGroup(group.id)}>Delete Group</button>
+						</div>
+					</div >
 				))
 			}
-			{!props.groups || props.groups.length === 0 &&
+			{
+				!props.groups || props.groups.length === 0 &&
 				<>
 					<h2 className="text-2xl font-semibold">No groups found</h2>
 					<p className="text-gray-500">Create a group to start a session...</p>
